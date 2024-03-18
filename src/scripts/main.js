42cf0e47bd6c5a282bc7e628eec4055a25353249
agbjunior <adilsonguimaraes1@hotmail.com>
2024-03-18 11:38:34 +0000

    commit inicial

---
+++ src/scripts/main.js
@@ -0,0 +1,7 @@
+function alternarVisibilidade(){
+    let test = document.querySelector('#test')
+    let botao = document.querySelector('#mybutton')
+
+    test.style.display = (test.style.display === "none") ? "block" : "none"
+    
+}
